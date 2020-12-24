2e2ed17a9ff9de621baf79344174e479a4708855
Justin Lowen <Justin.G.Lowen@gmail.com>
2020-12-24 08:23:20 -0600

    Day 23 Complete - Execution Could Be Faster Using Ptrs

--- Challenges/23/challenge23-2.py
+++ Challenges/23/challenge23-2.py
@@ -1,6 +1,24 @@
 import collections
 from pprint import pprint
 import sys
+import timeit
+import time
+
+def time_it(f):
+    time_it.active = 0
+
+    def tt(*args, **kwargs):
+        time_it.active += 1
+        t0 = time.time()
+        tabs = '\t'*(time_it.active - 1)
+        name = f.__name__
+        print('{tabs}Executing <{name}>'.format(tabs=tabs, name=name))
+        res = f(*args, **kwargs)
+        print('{tabs}Function <{name}> execution time: {time:.3f} seconds'.format(
+            tabs=tabs, name=name, time=time.time() - t0))
+        time_it.active -= 1
+        return res
+    return tt
 
 def parseInput(filename):
     data = []
@@ -13,144 +31,108 @@ def parseInput(filename):
     return data
 
 
-
 class CrabbyLinkedList:
-    ''' All values are unique '''
     def __init__(self):
         self.head = None
-        self.tail = None 
+        self.tail = None
         self.currentcup = None
-        self.pickedup = []
-        self.pickedupval = []
-        self.destinationcup = None
-        self.maxvalue = float("-inf")
         self.lookup = {}
+        self.max = float("-inf")
+        self.length = 0
+
+    def get_list_order(self):
+        visited ={}
+        node = self.head
+        next_order_cw = []
+        while node not in visited:
+            visited[node] = True
+            next_order_cw.append(str(node.value))
+            node = node.next
+        print(" ".join(next_order_cw))
+
     
     def build(self, node):
+        self.length += 1
         self.lookup[node.value] = node
-        if node.value > self.maxvalue:
-            self.maxvalue = node.value
-        if self.head is None:
+        if node.value > self.max:
+            self.max = node.value
+        if self.head == None:
             self.head = node
             self.tail = node
             return
-        self.insertAfter(self.tail, node)
+        self.tail.next = node
+        node.prev = self.tail
         self.tail = node
-        # Crabby class circular LL change
 
     def complete_circle(self):
-        self.tail.next = self.head
         self.head.prev = self.tail
-        self.currentcup = self.head.value
-        visited = {}
+        self.tail.next = self.head
+        self.currentcup = self.head
+
+    def test_circle(self):
         node = self.head
+        visited = {}
+        test_length = 0
+        while node not in visited:
+            visited[node] = True
+            test_length += 1
+            node = node.next
+        if test_length != self.length:
+            print("Next direction not set up correctly.")
+        visited = {}
+        test_length = 0
+        while node not in visited:
+            visited[node] = True
+            test_length += 1
+            node = node.prev
+        if test_length != self.length:
+            print("Next direction not set up correctly.")
+        print("Tests complete!")
 
-    def pickup_3(self):
-        self.pickedup = []
-        self.pickedupval = []
-        start_node = self.lookup[self.currentcup].next
-        target_node = start_node
+    @time_it
+    def crab_shuffle(self):
+        # self.get_list_order()
+        # Pick 3
+        pickupval = []
+        pickednodes = []
+        pickupnode = self.currentcup.next
         for _ in range(3):
-            print(target_node.value)
-            next_target = target_node.next
-            self.remove(target_node)
-            self.pickedup.append(target_node)
-            self.pickedupval.append(target_node.value)
-            target_node = next_target
-        end_node = target_node
-        start_node.next = end_node
-        end_node.prev = start_node
-        print(f'cups in pickedup nodes {len(self.pickedup)}')
-        print(self.pickedupval)
-
-    def set_destinationcup(self):
-        self.destinationcup = None
-        destination = self.lookup[self.currentcup].value - 1
-        while destination <= 0 or destination in self.pickedupval:
+            pickupval.append(pickupnode.value)
+            pickednodes.append(pickupnode)
+            pickupnode = pickupnode.next
+        
+        # Pick Destination
+        destination = self.currentcup.value - 1
+        while destination <= 0 or destination in pickupval:
             if destination <= 0:
-                destination = self.maxvalue
+                destination = self.max
             else:
                 destination -= 1
-        print(f'destination: {destination}')
-        self.destinationcup = destination
-    
-    def move_3(self):
-        insertion_pos = self.lookup[self.destinationcup]
-        for step in range(3):
-            cup_to_move = self.pickedup.pop(0)
-            if step == 0:
-                final_node = insertion_pos.next
-                insertion_pos.next = cup_to_move
-                cup_to_move.prev = insertion_pos
-            else:
-                prev_cup.next = cup_to_move
-                cup_to_move.prev = prev_cup
-            prev_cup = cup_to_move
-        prev_cup.next = final_node
-        final_node.prev = prev_cup
-        print(f'cups left after moving {len(self.pickedup)}')
+        
+        # Move 3
+        # Pluck out 3, reconnect
+        left = self.currentcup
+        right = self.currentcup.next.next.next.next # skip over 3
+        left.next = right
+        right.prev = left
 
-    # def setTail(self, node):
-    #     self.lookup[node.value] = node
-    #     if self.tail is None:
-    #         self.setHead(node)
-    #         return
-    #     self.insertAfter(self.tail, node)
-    #     # Crabby class circular LL change
-    #     self.tail.next = self.head
-    
-    def insertBefore(self, node, nodeToInsert):
-        if nodeToInsert == self.head and nodeToInsert == self.tail:
-            return
-        self.remove(nodeToInsert)
-        nodeToInsert.prev = node.prev
-        nodeToInsert.next = node
-        if node.prev is None:
-            self.head = nodeToInsert
-        else:
-            node.prev.next = nodeToInsert
-        node.prev = nodeToInsert
+        # now make room for inserted cups
+        left = self.lookup[destination]
+        right = left.next
+        prev_cup = left
+        for _ in range(3):
+            inserted_cup = pickednodes.pop(0)
+            prev_cup.next = inserted_cup
+            inserted_cup.prev = prev_cup
+            prev_cup = inserted_cup
+        prev_cup.next = right
+        right.prev = prev_cup
 
-    def insertAfter(self, node, nodeToInsert):
-        self.remove(nodeToInsert)
-        nodeToInsert.prev = node
-        nodeToInsert.next = node.next
-        node.next = nodeToInsert
-
-    def insertAtPosition(self, position, nodeToInsert):
-        self.lookup[nodeToInsert.value] = nodeToInsert
-        if position == 1:
-            self.setHead(nodeToInsert)
-            return
-        node = self.head
-        currentPosition = 1
-        while node is not None and currentPosition != position:
-            node = node.next
-            currentPosition += 1
-        if node is not None:
-            self.insertBefore(node, nodeToInsert)
-        else:
-            self.setTail(nodeToInsert)
-    
-    def removeNodesWithValue(self, value):
-        node = self.head
-        while node.next is not self.head:
-            nodeToRemove = node
-            node = node.next
-            if nodeToRemove.value == value:
-                self.remove(nodeToRemove)
-        self.lookup.pop(value, None)
-    
-    def remove(self, node):
-        self.removeNodeBindings(node)
-
-    def removeNodeBindings(self, node):
-        if node.prev is not None:
-            node.prev.next = node.next
-        if node.next is not None:
-            node.next.prev = node.prev
-        node.prev = None
-        node.next= None
+        # finally update current cup to it's next cup
+        self.currentcup = self.currentcup.next
+        # print(f'current: {self.currentcup.value}')
+        # print(f'pick up: {pickupval}')
+        # print(f'destination: {destination}')
 
 
 class CrabbyNode:
@@ -160,39 +142,32 @@ class CrabbyNode:
         self.prev = None
 
 def crabcups1mil(crabby_llist):
-    for move in range(10000000 + 1):
-        print(f'-- move {move + 1} --')
-        visited = {}
-        current_order = []
-        node = crabby_llist.lookup[3]
-        while node not in visited:
-            visited[node] = True
-            if node.value == crabby_llist.currentcup:
-                current_order.append(f'({node.value})')
-            else:
-                current_order.append(node.value)
-            node = node.prev
-        print(current_order)
-        crabby_llist.pickup_3()
-        crabby_llist.set_destinationcup()
-        crabby_llist.move_3()
-        crabby_llist.currentcup = crabby_llist.lookup[crabby_llist.currentcup].next.value
-        print(crabby_llist.lookup[crabby_llist.currentcup].value)
-    target = crabby_llist.lookup[1]
-    onebefore = target.prev
-    twobefore = target.prev.prev
-    print(onebefore)
-    print(twobefore)
+    for move in range(10000000):
+        start = timeit.default_timer()
+        if move % 1000000 == 1:
+            end = timeit.default_timer()
+            print(f'1000000 cycles took {end - start}')
+            start = timeit.default_timer()
+        # print(f'-- move {move+1} --')
+        crabby_llist.crab_shuffle()
+    # print('-- final --')
+    # crabby_llist.get_list_order()
+    one_node = crabby_llist.lookup[1]
+    print(one_node.next.value)
+    print(one_node.next.next.value)
+    
 
     
 example = [3,8,9,1,2,5,4,6,7]
 input = [3,2,7,4,6,5,1,8,9]
-# crabby_input = input + [num for num in range(100 + 1) if num > 9]  
-crabby_input = example
+crabby_input = input + [num for num in range(1000000 + 1) if num > 9]  
 # print(crabby_input)  
 crabby_llist = CrabbyLinkedList()
+start = timeit.default_timer()
 for num in crabby_input:
     crabby_llist.build(CrabbyNode(num))
+end = timeit.default_timer()
+print(f'build took {end - start}')
 crabby_llist.complete_circle()
 # print(crabby_llist.lookup)
 
